7deecee3105b71221f304992adccaa52fe0795ff
Dmytro <gontarev.dmitry@gmail.com>
2019-08-24 16:27:55 +0300

    Commit Gradle and java

---
+++ src/main/java/Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+    public static void main(String[] args)
+    {
+        Integer a=1;
+        Integer b=2;
+
+        System.out.println(a+b);
+        System.out.println("Hello, World");
+    }
+}
