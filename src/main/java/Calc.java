25a4d10e5ec02522d09a6902cf39ac232b367d31
Dmytro <gontarev.dmitry@gmail.com>
2019-08-24 22:37:08 +0300

    Comments added Hello class deleted

--- src/main/java/Calc.java
+++ src/main/java/Calc.java
@@ -3,31 +3,41 @@ import java.util.Scanner;  // Import the Scanner class
 public class Calc {
 
     public static void SimpleSum() {
+        // Variables definition
         Integer a=1;
         Integer b=2;
         Integer sum = a + b;
+
+        // Output
         System.out.println("Your choice is just sum of 2 predefined ints.");
         System.out.println("First number is " + a + " and the second is " + b + ".");
         System.out.println("The sum is: " + sum);
     }
 
     public static void SimpleCalc() {
+        // Variables initialization
         Integer a;
         Integer b;
         Integer result;
         String operation;
 
-        Scanner calcInput = new Scanner(System.in);  // Create a Scanner object
+        // Creation of a Scanner object
+        Scanner calcInput = new Scanner(System.in);
 
+        // Output
         System.out.println("Your choice is simple calc.");
         System.out.println("It can only operate simple operations with 2 int numbers.");
+        // Prompt for a required operation
         System.out.println("Enter operation (+, -, *. /):");
         operation = calcInput.nextLine();
-        System.out.println("Enter the first number:");
+        // Prompt for a first number
+        System.out.println("Enter a first number:");
         a = calcInput.nextInt();
-        System.out.println("Enter the second number:");
+        // Prompt for a second number
+        System.out.println("Enter a second number:");
         b = calcInput.nextInt();
 
+        // Operation perform
         switch (operation) {
             case "+":
                 result = Math.addExact(a, b);
@@ -51,14 +61,18 @@ public class Calc {
     }
 
     public static void Boobs() {
+        // Variables initialization
         String answer;
 
-        Scanner boobsInput = new Scanner(System.in);  // Create a Scanner object
+        // Creation of a Scanner object
+        Scanner boobsInput = new Scanner(System.in);
 
+        // Prompt for an answer
         System.out.println("Your choice is ASCII boobs.");
         System.out.println("Would you give me \"A\" grade for the first task? (y/n)");
         answer = boobsInput.nextLine();
 
+        // Processing of an answer
         switch (answer) {
             case "y":
                 System.out.println("( . ) ( . )");
@@ -73,15 +87,19 @@ public class Calc {
 
     public static void main(String[] args)
     {
+        // Variables initialization
         Integer keyInput;
 
-        Scanner input = new Scanner(System.in);  // Create a Scanner object
+        // Creation of a Scanner object
+        Scanner input = new Scanner(System.in);
+
+        // Promt of an option
         System.out.println("Enter \"1\" if you want just sum of 2 predefined ints.");
         System.out.println("Enter \"2\" if you want simple calc.");
         System.out.println("Enter \"3\" if you want ASCII boobs.");
-
         keyInput = input.nextInt();
 
+        // Calling a method depending on selected option
         switch (keyInput) {
             case 1:
                 SimpleSum();
